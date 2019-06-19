cb41635ff8887e188a5e100b628b378264ef9722
quenio <queniodossantos@gmail.com>
2019-06-19 16:01:04 -0300

    Input: Defined the placeholder and variable attributes.

--- app/input.rb
+++ app/input.rb
@@ -21,13 +21,34 @@
 #
 
 require 'view'
+require 'selectable'
 
 class Input < View
+  include Selectable
+
+  attr_reader :placeholder, :variable
+
   def initialize(params = {})
     super(params.merge(
       tag: 'input',
-      fixed_style_classes: %w[form-control w-25 m-1 mb-2],
-      placeholder: 'Input'
+      fixed_style_classes: %w[form-control m-1 mb-2] + (params[:fixed_style_classes] || [])
     ))
+    self.placeholder = params[:placeholder]
+    self.variable = params[:variable]
+    support_selection unless params[:non_selectable]
+  end
+
+  def attributes
+    [method('variable='.to_sym)]
+  end
+
+  def placeholder=(value)
+    @placeholder = value
+    @element.setAttribute :placeholder, @placeholder if @placeholder
+  end
+
+  def variable=(value)
+    @variable = value
+    @element.setAttribute :placeholder, @variable || '' unless @placeholder
   end
 end
