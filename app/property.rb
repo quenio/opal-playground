38431c4dd2402cd922b2d337bb821a0fad74e637
quenio <queniodossantos@gmail.com>
2019-06-20 18:31:29 -0300

    Defined the Property class.

---
+++ app/property.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+#--
+# Copyright (c) 2019 Quenio Cesar Machado dos Santos
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+#
+
+class Property
+  def self.list(receiver, names)
+    names.map do |name|
+      Property.new(receiver, name)
+    end
+  end
+
+  attr_reader :name
+
+  def initialize(receiver, name)
+    @name = name.to_s.capitalize
+    @getter = receiver.method(name.to_sym)
+    @setter = receiver.method("#{name.to_s}=".to_sym)
+  end
+
+  def value
+    @getter.call
+  end
+
+  def value=(value)
+    @setter.call(value)
+  end
+end
