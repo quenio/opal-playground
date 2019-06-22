0ceff10879acae1d1b74d58a81ee95d2b74b30c5
quenio <queniodossantos@gmail.com>
2019-06-22 16:36:20 -0300

    Defined ExpressionParser.

---
+++ lib/expression_parser.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+#--
+# Copyright (c) 2019 Quenio Cesar Machado dos Santos
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+#
+
+require 'equation'
+
+class ExpressionParser
+  EQUALITY_OPERATOR = '='
+
+  def parse(text)
+    left, right = text.split(EQUALITY_OPERATOR).map(&:strip)
+    left_var = $model.find_or_create_variable(left)
+    right_var = $model.find_or_create_variable(right)
+    [Equation.new(left: left_var, right: right_var)]
+  end
+end
